93eb4e731a5649e38a0cb6a777353c3b4f7beb64
ChristopherMorriss <chrismorriss711@gmail.com>
2024-09-26 12:53:38 +0100

    Added seeder for user creation

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -34,7 +34,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
-            'admin' => false
+            //'admin' => false
         ];
     }
 
@@ -47,10 +47,10 @@ class UserFactory extends Factory
             'email_verified_at' => null,
         ]);
     }
-    public function admin(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'admin' => true,
-        ]);
-    }// User::factory()->admin()->create()
+    // public function admin(): static
+    // {
+    //     return $this->state(fn (array $attributes) => [
+    //         'admin' => true,
+    //     ]);
+    // }// User::factory()->admin()->create()
 }
